bf435a21b874fad76f891f91c96acbdeca9721e3
Aakash Dutta Subedi <aakashsubedi80@gmail.com>
2024-01-14 13:28:34 +0545

    implemented working gmae . removed unnecessary css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Square from "./Square";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Board() {
+  const [isXturn, setXturn] = useState(true);
+  const [squareValue, setSquareValue] = useState(Array(9).fill(""));
+  function handleClick(index: number) {
+    const array = squareValue.slice();
+
+    if (
+      array[index] == "X" ||
+      array[index] == "O" ||
+      calculateWinner(squareValue)
+    )
+      return;
+
+    if (isXturn) {
+      array[index] = "X";
+      setXturn(false);
+    } else {
+      array[index] = "O";
+      setXturn(true);
+    }
+    setSquareValue(array);
+  }
+
+  const winner = calculateWinner(squareValue);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next turn: " + (isXturn ? "X" : "O");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="board-row">
+        <Square value={squareValue[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squareValue[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squareValue[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="board-row">
+        <Square value={squareValue[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squareValue[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squareValue[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="board-row">
+        <Square value={squareValue[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squareValue[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squareValue[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+      <div className="status">{status}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default Board;
+
+function calculateWinner(squareValue: any[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squareValue[a] &&
+      squareValue[a] === squareValue[b] &&
+      squareValue[a] === squareValue[c]
+    ) {
+      return squareValue[a];
+    }
+  }
+  return null;
+}
